c8216db33248855696060a8bed986952ca4642c6
OGerison <56107729+OGerison@users.noreply.github.com>
2021-06-03 12:01:22 +0300

    Update settings.py

--- otimexam/settings.py
+++ otimexam/settings.py
@@ -23,11 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%8-)2^7o2-j7)vz6hyq^*v4-_1@=)0o%hra1!1ahhje8h!*$iy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','otimexam.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 
@@ -45,7 +44,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
